91db496109d0f19d55d3cca0f2f959e496ce9740
Filip Mularski <mularskif@gmail.com>
2021-03-10 21:32:03 +0100

    created basic user registration form

---
+++ practice/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = UserCreationForm()
+
+    return render(request, 'practice/register.html', {'form': form})
